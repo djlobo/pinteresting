d7c4432bb6b762725fe60601aa6cb308f286ef8a
David Lobo <davidjlobo@gmail.com>
2019-05-01 01:36:08 -0400

    added a hard to follow steps and installed Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #DL 4/30 required for Heroku; not set this to actual host
+  config.action_mailer.default_url_options = { host: 'http://startupnoob.com/' }
 end
